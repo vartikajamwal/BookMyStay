6a0472d08af462c6f733fbf455950b3723dd848c
vartikajamwal <vj2081@srmist.edu.in>
2026-03-13 14:45:57 +0530

    UC 1 - Application Entry & Welcome Message

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,2 +1,9 @@
 public class BookMyStay {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Book My Stay");
+        System.out.println("Hotel Booking System v1.0");
+
+    }
 }
